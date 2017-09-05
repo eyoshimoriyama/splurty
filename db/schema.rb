8a4d98d192c22873fc1fbe398f2a1df9aa64cf5a
Yoshi Moriyama <eyoshimoriyama@gmail.com>
2017-09-05 01:19:41 +0000

    Added quotes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905003829) do
+ActiveRecord::Schema.define(version: 20170905010703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "quotes", force: :cascade do |t|
     t.string   "saying"
-    t.string   "authore"
+    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
